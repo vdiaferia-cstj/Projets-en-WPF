5c3b33c66f79b9179353d76d7eb1a4935fbe28e9
bigdiaf <1967480@csjj.qc.ca>
2022-11-15 13:25:24 -0500

    Modified marketplace, added Auto class

---
+++ TP2/Classes/Auto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    public class Auto
+    {
+        public string Image;
+        public DateTime Date;
+        public double Price;
+        public string Maker;
+        public string Brand;
+        public int Year;
+        public int Odometer;
+
+    }
+}
